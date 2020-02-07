9af3d4d8d474cf3cf60632f429ac637d1812d7f5
Henrique Pereira <hlcpereira@pixelexperience.org>
2020-02-07 16:40:16 -0300

    sm6150-common: power: Bring in fts definition
    * Tucana also has this touch device apart from gt9886
    
    add device 7: /dev/input/event4
      name:     "fts"
    
    Signed-off-by: Henrique Pereira <hlcpereira@pixelexperience.org>
    Change-Id: I75ab85e1badbb4bc22fec073c4382e4da1634593

--- power-libperfmgr/Power.cpp
+++ power-libperfmgr/Power.cpp
@@ -194,7 +194,8 @@ int open_ts_input() {
 
                 fd = open(absolute_path, O_RDWR);
                 if (ioctl(fd, EVIOCGNAME(sizeof(name) - 1), &name) > 0) {
-                    if (strcmp(name, "fts_ts") == 0 || strcmp(name, "goodix_ts") == 0 ||
+                    if (strcmp(name, "fts_ts") == 0 || strcmp(name, "fts") == 0 ||
+                            strcmp(name, "goodix_ts") == 0 ||
                             strcmp(name, "NVTCapacitiveTouchScreen") == 0)
                         break;
                 }
