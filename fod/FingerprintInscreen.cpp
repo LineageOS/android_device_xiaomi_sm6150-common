6e576c389882dbd5fea92fc7f2bad5a2e8f757cf
PIPIPIG233666 <pig.priv@gmail.com>
2020-02-16 19:06:49 -0500

    sm6150-common: fod: Disable dc dimming onShowFODView
    Change-Id: Ieb632d4a0a8a633d7e1f9bc554d249b276c2225a

--- fod/FingerprintInscreen.cpp
+++ fod/FingerprintInscreen.cpp
@@ -32,6 +32,9 @@
 #define DISPPARAM_HBM_FOD_ON "0x20000"
 #define DISPPARAM_HBM_FOD_OFF "0xE0000"
 
+#define FLICKER_FREE_NODE                                                      \
+  "/sys/devices/platform/soc/soc:qcom,dsi-display/ea_enable"
+
 #define Touch_Fod_Enable 10
 #define Touch_Aod_Enable 11
 
@@ -48,9 +51,31 @@ namespace V1_0 {
 namespace implementation {
 
 template <typename T>
+static T get(const std::string& path, const T& def) {
+    std::ifstream file(path);
+    T result;
+
+    file >> result;
+    return file.fail() ? def : result;
+}
+
+template <typename T>
 static void set(const std::string& path, const T& value) {
     std::ofstream file(path);
     file << value;
+}
+
+void dcdimmingcheck(bool enabled) {
+    auto DcDimmingStatus = get<std::string>(FLICKER_FREE_NODE), "");
+    if (DcDimmingStatus == 1) {
+        enabled == true;
+        set(FLICKER_FREE_NODE, 0)
+    }
+    if (enabled == true) {
+        set(FLICKER_FREE_NODE, 1);
+    } else {
+        set(FLICKER_FREE_NODE, 0);
+    }
 }
 
 FingerprintInscreen::FingerprintInscreen() {
@@ -95,6 +120,7 @@ Return<void> FingerprintInscreen::onRelease() {
 
 Return<void> FingerprintInscreen::onShowFODView() {
     TouchFeatureService->setTouchMode(Touch_Fod_Enable, 1);
+    dcdimmingcheck();
     this->mFodCircleVisible = true;
     return Void();
 }
@@ -102,6 +128,7 @@ Return<void> FingerprintInscreen::onShowFODView() {
 Return<void> FingerprintInscreen::onHideFODView() {
     set(DISPPARAM_PATH, DISPPARAM_HBM_FOD_OFF);
     TouchFeatureService->resetTouchMode(Touch_Fod_Enable);
+    dcdimmingcheck();
     this->mFodCircleVisible = false;
     return Void();
 }
