0891c9e1766bfedf032708bdce719e6b224246ca
danielml <daniel@danielml.dev>
2023-06-03 22:16:39 +0200

    Revert "sm6150-common: parts: Enable doze_status on screen off"
    * Manually setting the doze status before entering the low-power mode
      makes the doze mode not to be set correctly on sweet.
    
    * The doze status is automatically set when entering the low-power mode
    
    This reverts commit 10f101e14301c9a34cb5c74489b439edb6088aa6.
    
    Change-Id: I4dfee2ffb351b9bc426268e437a26f9eaefec319

--- parts/src/org/lineageos/settings/doze/DozeUtils.java
+++ parts/src/org/lineageos/settings/doze/DozeUtils.java
@@ -60,11 +60,6 @@ public final class DozeUtils {
     protected static final String DOZE_MODE_HBM = "1";
     protected static final String DOZE_MODE_LBM = "0";
 
-    private static final String DOZE_STATUS_PATH =
-            "/sys/devices/platform/soc/soc:qcom,dsi-display/doze_status";
-    protected static final String DOZE_STATUS_ENABLED = "1";
-    protected static final String DOZE_STATUS_DISABLED = "0";
-
     protected static final String DOZE_BRIGHTNESS_LBM = "0";
     protected static final String DOZE_BRIGHTNESS_HBM = "1";
     protected static final String DOZE_BRIGHTNESS_AUTO = "2";
@@ -86,7 +81,9 @@ public final class DozeUtils {
     }
 
     public static void checkDozeService(Context context) {
-        if (isDozeEnabled(context) && (isAlwaysOnEnabled(context) || sensorsEnabled(context))) {
+        if (isDozeEnabled(context)
+                && (!isAlwaysOnEnabled(context) || isDozeAutoBrightnessEnabled(context))
+                && sensorsEnabled(context)) {
             startService(context);
         } else {
             stopService(context);
@@ -155,10 +152,6 @@ public final class DozeUtils {
 
     protected static boolean setDozeMode(String value) {
         return FileUtils.writeLine(DOZE_MODE_PATH, value);
-    }
-
-    protected static boolean setDozeStatus(String value) {
-        return FileUtils.writeLine(DOZE_STATUS_PATH, value);
     }
 
     protected static boolean isDozeAutoBrightnessEnabled(Context context) {
