4bd224bdbb4131a965e288ba68ce141a9baf7e46
Alexander Baransky <alexander.baranskiy2006@gmail.com>
2024-01-11 14:08:14 +0300

    sm6150-common: Initial support for tucana
    Change-Id: I6b2c62dbc3030fb2410b5274bef279c4a66d26a6

--- Android.mk
+++ Android.mk
@@ -16,7 +16,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter courbet davinci phoenix surya sweet toco violet,$(TARGET_DEVICE)),)
+ifneq ($(filter courbet davinci phoenix surya sweet toco tucana violet,$(TARGET_DEVICE)),)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
