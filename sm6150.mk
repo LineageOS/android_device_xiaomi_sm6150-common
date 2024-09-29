69051e942c264d5189bda41189e80506c7d91923
danielml <daniel@danielml.dev>
2024-09-29 18:17:18 +0200

    sm6150-common: Skip building unneeded libraries
    Change-Id: I0a34776c3354e37c6ae3d2b9eca9150d902b99fc
    Signed-off-by: danielml <daniel@danielml.dev>

--- sm6150.mk
+++ sm6150.mk
@@ -258,9 +258,7 @@ PRODUCT_COPY_FILES += \
 
 # RIL
 PRODUCT_PACKAGES += \
-    CarrierConfigOverlay \
-    libril \
-    librilutils
+    CarrierConfigOverlay
 
 # Recovery
 PRODUCT_PACKAGES += \
@@ -341,9 +339,6 @@ PRODUCT_COPY_FILES += \
     vendor/qcom/opensource/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 
 # Vulkan
-PRODUCT_PACKAGES += \
-    libvulkan
-
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.vulkan.compute-0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.compute.xml \
     frameworks/native/data/etc/android.software.vulkan.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml \
