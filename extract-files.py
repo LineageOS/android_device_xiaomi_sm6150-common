d7eef58c830eb01e58433dd600779ccf8e059cc3
Giovanni Ricca <giovanniricca@proton.me>
2024-11-12 16:22:26 +0100

    sm6150-common: Move to 64bit wfdservice
    Co-authored-by: Alexander Koskovich <zvnexus@outlook.com>
    Co-authored-by: Alexander Winkowski <dereference23@outlook.com>
    Change-Id: I9d1e97ea8ac8cc75bdb6c29d6dfc0008e57a660d

--- extract-files.py
+++ extract-files.py
@@ -47,6 +47,8 @@ lib_fixups: lib_fixups_user_type = {
 
 
 blob_fixups: blob_fixups_user_type = {
+    'system_ext/etc/init/wfdservice.rc': blob_fixup()
+        .regex_replace(r'(start|stop) wfdservice\b', r'\1 wfdservice64'),
     'system_ext/lib64/libwfdnative.so': blob_fixup()
         .remove_needed('android.hidl.base@1.0.so'),
     ('vendor/lib64/mediadrm/libwvdrmengine.so', 'vendor/lib64/libwvhidl.so'): blob_fixup()
