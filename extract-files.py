690fa1675ef130a2eece0bc0a880240cebb35650
Nolen Johnson <johnsonnolen@gmail.com>
2024-11-30 17:09:15 -0500

    sm6150-common: fix libmmosal ELF checks
    Change-Id: I4f016772a486d5fe894e0c13aece6a49bada38f5

--- extract-files.py
+++ extract-files.py
@@ -38,6 +38,7 @@ lib_fixups: lib_fixups_user_type = {
     **lib_fixups,
     (
         'com.qualcomm.qti.dpm.api@1.0',
+        'libmmosal',
         'vendor.qti.hardware.fm@1.0',
         'vendor.qti.imsrtpservice@3.0',
     ): lib_fixup_vendor_suffix,
