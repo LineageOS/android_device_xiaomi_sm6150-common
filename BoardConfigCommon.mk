3556ea30e9d5952c7d0999f52cf8678f71f7565a
basamaryan <basam.aryan@gmail.com>
2025-01-02 20:36:43 +0530

    sm6150-common: Bring back QCOM WFD
    This reverts commit 4a838e1a9bde7fe16c802e26f1c4c9d27e8b200f and d5d80a5347fc9a38cf3a5c08ed38a65e565c3b01.
    
    Change-Id: Ib6b2ffc341fc22511ebacdd6b716257090209b3b

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -64,6 +64,7 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/configs/hidl/manifest.xml
 DEVICE_MANIFEST_FILE += hardware/qcom-caf/sm8150/media/conf_files/sm6150/c2_manifest.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
+ODM_MANIFEST_FILES += $(COMMON_PATH)/configs/hidl/manifest-qva.xml
 
 # Kernel
 BOARD_KERNEL_BASE := 0x00000000
--- sm6150.mk
+++ sm6150.mk
@@ -375,6 +375,17 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.rtt.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.rtt.xml \
     frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.xml
 
+# WiFi Display
+PRODUCT_PACKAGES += \
+    libavservices_minijail_32 \
+    libdisplayconfig.qti \
+    libnl \
+    libqdMetaData \
+    libwfdaac_vendor
+
+PRODUCT_BOOT_JARS += \
+    WfdCommon
+
 # WiFi firmware symlinks
 PRODUCT_PACKAGES += \
     firmware_wlan_mac.bin_symlink \
