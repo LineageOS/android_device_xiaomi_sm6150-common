5c16995d5118ecaa6978fc06fb998c54754c6973
basamaryan <basam.aryan@gmail.com>
2025-01-22 21:58:48 -0500

    sm6150-common: Decommonize public.libraries.txt
    Change-Id: I3b9b1195198f975bc19a6618b13b8b6d8db0160a

--- sm6150.mk
+++ sm6150.mk
@@ -28,10 +28,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 # Add common definitions for Qualcomm
 $(call inherit-product, hardware/qcom-caf/common/common.mk)
 
-# Additional native libraries
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
-
 # AID/fs configs
 PRODUCT_PACKAGES += \
     fs_config_files
