10dbf849a5c0d76e311dded256ddad5eec5c087b
Giovanni Ricca <giovanniricca@proton.me>
2025-02-08 21:04:28 +0100

    sm6150-common: media: Reapply media codecs settings
    Taken from media_codecs_vendor.xml
    
    Change-Id: Ie61482799d082c9a176025da7c428c46aa27d746

--- sm6150.mk
+++ sm6150.mk
@@ -213,7 +213,6 @@ PRODUCT_PACKAGES += \
 
 # Media
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
     $(LOCAL_PATH)/configs/media/media_codecs_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_c2.xml \
     $(LOCAL_PATH)/configs/media/media_codecs_performance_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance_c2.xml \
     $(LOCAL_PATH)/configs/media/media_profiles.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml
