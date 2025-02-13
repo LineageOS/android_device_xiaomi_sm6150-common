ef4b24e099b911b5d4f30c2d9819ff1d0c85da88
Yumi Yukimura <me.cafebabe@gmail.com>
2025-02-13 15:29:15 +0800

    sm6150-common: Do not build LiveDisplay HAL by default
    * violet supports none of the modes that it supports.
    
    Change-Id: I153a9460a1b191556e3e686a7489b952fed0c254

--- sm6150.mk
+++ sm6150.mk
@@ -207,10 +207,6 @@ PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
 
-# LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.1-service.xiaomi_sm6150
-
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
