58c0a20e71ebf0f9eabcdd9334862f68df1b606b
Yumi Yukimura <me.cafebabe@gmail.com>
2025-02-13 15:12:45 +0800

    sm6150-common: Migrate to QTI USB Gadget AIDL HAL
    Change-Id: Ie5a4789690a917f7b37d4e27668f99be8efd35c5

--- sm6150.mk
+++ sm6150.mk
@@ -327,7 +327,9 @@ PRODUCT_COPY_FILES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.3-service.dual_role_usb
+    android.hardware.usb@1.3-service.dual_role_usb \
+    android.hardware.usb.gadget-service.qti \
+    usb_compositions.conf
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
