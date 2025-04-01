e0f0c75e04b475bdd84945578c7443a961462253
LuK1337 <priv.luk@gmail.com>
2025-04-01 16:11:23 +0200

    sm6150-common: Add libaudiofoundation from stock for WFD
    e69a5be2449122f90220151f9d01ace15221facf in system/hardware/interfaces
    broke the ABI ;'(
    
    Change-Id: I60691f2bab3f52fd9ade36ed6fcf6a2ef13a18c9

--- extract-files.py
+++ extract-files.py
@@ -54,6 +54,7 @@ blob_fixups: blob_fixups_user_type = {
     'system_ext/lib64/libwfdnative.so': blob_fixup()
         .remove_needed('android.hidl.base@1.0.so'),
     'system_ext/lib64/libwfdservice.so': blob_fixup()
+        .replace_needed('libaudiofoundation.so', 'libwfdaudiofoundation.so')
         .replace_needed('android.media.audio.common.types-V2-cpp.so', 'android.media.audio.common.types-V4-cpp.so'),
     ('vendor/lib64/mediadrm/libwvdrmengine.so', 'vendor/lib64/libwvhidl.so'): blob_fixup()
         .add_needed('libcrypto_shim.so'),
