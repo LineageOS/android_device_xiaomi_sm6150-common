c166af78895885f80cd14deb19a97585fe0b6cf7
imranpopz <imranpopz32@gmail.com>
2025-12-30 06:13:26 +0000

    sm6150-common: Build vendor variant of libjxl
    01-15 23:51:58.070  8913  8913 E CamX    : [ERROR][UTILS  ] camxosutilslinux.cpp:898 LibMap() dlopen: dlopen failed: library "libjxl.so" not found: needed by /vendor/lib64/libdng_sdk.so in namespace (default), on '/vendor/lib64/camera/components/com.qti.node.watermark.so'
    
    Change-Id: Ib00c760091b372c951d054eeca4da353bda1bb30

--- sm6150.mk
+++ sm6150.mk
@@ -90,7 +90,8 @@ $(call soong_config_set_bool,camera,override_format_from_reserved,true)
 
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service_64
+    android.hardware.camera.provider@2.4-service_64 \
+    libjxl.vendor
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
